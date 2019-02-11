2204f7d35b387264961f3a5895afcaa45118ce01
alessandrosuprani <alessandro.suprani.ittbpascal@gmail.com>
2019-02-11 08:49:18 +0100

    Parte Client : aggiornamento interfaccia, primi punti completati

---
+++ SOAPClient/easy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOAPClient
+{
+    class easy
+    {
+        void trppfrt()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                i = i - 1;
+            }
+        }
+    }
+}
